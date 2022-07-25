3b398b30db838fa4703051495f122f209133f476
PersesTitan <torampersesTitan@gmail.com>
2022-07-25 21:15:55 +0900

    create main page component

---
+++ src/components/otl-main.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>OTLanguage 페이지에 어서오세요.</h1>
+  <ul>
+    <li>
+      <a>OTLanguage</a>
+    </li>
+    <li>
+      <a>Poison</a>
+    </li>
+    <li>
+      <a>Music</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "otl-main"
+}
+</script>
+
+<style scoped>
+
+</style>
