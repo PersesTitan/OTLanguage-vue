3c484fc041280a5566372f285d8fcb0915d9e568
PersesTitan <torampersesTitan@gmail.com>
2022-07-25 21:15:42 +0900

    create otl-code component

---
+++ src/components/edit/otl-code.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="code-pos">
+    <code>
+      <slot/>
+    </code>
+  </p>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.code-pos {
+  display: flex;
+  background: lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  overflow: auto;
+  white-space: nowrap;
+}
+</style>
