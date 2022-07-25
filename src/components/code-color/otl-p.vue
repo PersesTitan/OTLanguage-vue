3c00d4fa5970d23e5a6d7daa419b529cdba463ee
PersesTitan <torampersesTitan@gmail.com>
2022-07-25 21:16:11 +0900

    create p color component

---
+++ src/components/code-color/otl-p.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>
+    <slot/>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "otl-p"
+}
+</script>
+
+<style scoped>
+span {
+  color: mediumpurple;
+  font-weight: bolder;
+}
+</style>
