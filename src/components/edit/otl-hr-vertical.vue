00b2ff67e7ed4b80e18c8d30f4249305132a5eb8
PersesTitan <torampersesTitan@gmail.com>
2022-07-26 23:27:23 +0900

    create otl-hr-vertical.vue

---
+++ src/components/edit/otl-hr-vertical.vue
@@ -0,0 +1,17 @@
+<template>
+  <hr class="vertical">
+</template>
+
+<script>
+export default {
+  name: "otl-hr-vertical"
+}
+</script>
+
+<style scoped>
+hr.vertical {
+  height: 50px;
+  width: 0;
+  border-right: 0;
+}
+</style>
