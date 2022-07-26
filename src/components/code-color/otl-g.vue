9178465f1b9d4a19869a87fb572b18b7ad8e5279
PersesTitan <torampersesTitan@gmail.com>
2022-07-26 23:27:03 +0900

    create otl-g.vue

---
+++ src/components/code-color/otl-g.vue
@@ -0,0 +1,18 @@
+<template>
+  <span class="g">
+    <slot/>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "otl-g"
+}
+</script>
+
+<style scoped>
+.g {
+  color: forestgreen;
+  font-weight: bolder;
+}
+</style>
