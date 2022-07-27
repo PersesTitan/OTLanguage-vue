d6745804ef6433400cfa2128ee2aa4e43aa38d75
PersesTitan <torampersesTitan@gmail.com>
2022-07-27 15:15:12 +0900

    create subtotal

---
+++ src/components/otl-subtotal.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>소계 페이지</h1>
+  <p>OTLanguage는 Java기반으로 제작된 난해한 프로그래밍 언어입니다.</p>
+
+  <h2>이름의 유래</h2>
+  <p>OTL이라는 절망 이모티콘과 Language라는 언어라는 단어의 합성어 입니다.</p>
+
+  <h2>특징</h2>
+  <p>주로 언어가 이모티콘으로 이루어져 있는 것이 특징입니다.</p>
+  <p>기본 단위가 문자열입니다.</p>
+</template>
+
+<script>
+export default {
+  name: "otl-subtotal"
+}
+</script>
+
+<style scoped>
+
+</style>
