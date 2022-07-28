972f0ff127eff70f3a7b60fb3a627a5aeed4cf3f
PersesTitan <torampersesTitan@gmail.com>
2022-07-28 18:16:52 +0900

    create otl-o

---
+++ src/components/code-color/otl-o.vue
@@ -0,0 +1,18 @@
+<template>
+<span class="o">
+  <slot/>
+</span>
+</template>
+
+<script>
+export default {
+  name: "otl-o"
+}
+</script>
+
+<style scoped>
+.o {
+  color: darkorange;
+  font-weight: bolder;
+}
+</style>
