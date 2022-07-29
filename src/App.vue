df13928bd85ff83fffd2c8e09af5972feef743cc
PersesTitan <torampersesTitan@gmail.com>
2022-07-29 18:47:15 +0900

    update App code

--- src/App.vue
+++ src/App.vue
@@ -2,64 +2,46 @@
   <div v-if="path === ''">
     <otl-main/>
   </div>
-
   <div v-else>
-    <header class="header-top">
-      <img :src="require(`@/assets/banner/${ path }-banner.png`)" alt="/{{ path }}">
-      <ul class="menu">
-        <li>
-          <a href="/Music">Music</a>
-          <ul class="submenu">
-            <li><a>개요</a></li>
-            <li><a>다운로드</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="/Poison">Poison</a>
-          <ul class="submenu">
-            <li><a>개요</a></li>
-            <li><a>다운로드</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="/OTLanguage">OTLanguage</a>
-          <ul class="submenu">
-            <li><a href="/OTLanguage">소개</a></li>
-            <li><a href="/OTLanguage">개요</a></li>
-            <li><a href="/OTLanguage/download">다운로드</a></li>
-            <li><a href="/OTLanguage">문법</a></li>
-            <li><a href="/OTLanguage">정보</a></li>
-          </ul>
-        </li>
-      </ul>
+    <header>
+      <otl-header/>
     </header>
   </div>
 
-  <div v-if="subLink === 'download'">
-    <v-download/>
+  <div class="body">
+    <div v-if="path === 'OTLanguage'">
+      <otl-grammar v-if="subLink === 'grammar'"/>
+      <v-download v-else-if="subLink === 'download'"/>
+      <otl-information v-else-if="subLink === 'information'"/>
+      <otl-introduce v-else/>
+    </div>
+    <div v-else-if="path === 'Poison'">
+      <poison-information v-if="subLink === 'information'"/>
+    </div>
   </div>
-
-  <otl-code>
-    <otl-p>ㅅㅁㅅ</otl-p>&nbsp;안녕하세요.
-  </otl-code>
-
 </template>
 
 <script>
 
 // const mainColor = "#A900FF"
 import VDownload from "@/components/v-download";
-import OtlCode from "@/components/edit/otl-code";
-import OtlP from "@/components/code-color/otl-p"
 import OtlMain from "@/components/otl-main";
+import OtlHeader from "@/components/edit/otl-header"
+import OtlGrammar from "@/components/otl/otl-grammar";
+import OtlIntroduce from "@/components/otl/otl-introduce";
+import OtlInformation from "@/components/otl/otl-information";
+import PoisonInformation from "@/components/poison/poison-information";
 
 export default {
   name: 'App',
   components: {
+    PoisonInformation,
+    OtlInformation,
+    OtlIntroduce,
+    OtlGrammar,
     OtlMain,
-    OtlP,
-    OtlCode,
-    VDownload
+    VDownload,
+    OtlHeader
   },
   data() {
     return {
@@ -67,14 +49,30 @@ export default {
       music_link : "/Music",
       poison_link : "/Poison",
       kind : "OTLanguage",
-      path : window.location.pathname.split('/')[1],
+      path : window.location.pathname.split('/')[1], // OTLanguage, Poison, Music, ...
       subLink : window.location.pathname.split('/')[2] //download, subtotal, ...
     }
   },
+  // updated() {
+  //   const ele = document.getElementsByTagName('body')
+  //   const app = document.getElementById('app')
+  //   if (this.path === '') {
+  //     ele.style.marginTop = 0
+  //     ele.style.margin = 0
+  //     app.style.marginTop = 0
+  //   } else {
+  //     ele.style.marginTop = 70
+  //     app.style.marginTop = 60
+  //   }
+  // }
+
   updated() {
-    const ele = document.getElementsByTagName('body')
-    if (this.path === '') ele.style.marginTop = '0px'
-    else ele.style.marginTop = '70px'
+    const app = document.getElementById('app')
+    if (this.path === '') {
+      app.style.marginTop = "0px"
+    } else {
+      app.style.marginTop = "60px"
+    }
   }
 }
 </script>
@@ -84,71 +82,17 @@ body {
   margin: 0;
 }
 
-/*배너 정의*/
-.header-top {
-  position: fixed;
-  display: flex;
-  left: 0;
-  right: 0;
-  top: 0;
-  justify-content: space-between;
-  align-items: center;
-  background: #ce80ff;
-  height: 70px;
-}
-
-.header-top img {
-  margin-left: 10px;
-  height: 70px;
-  left: 0;
-  top: 0;
-}
-
-/*메뉴*/
-.menu {
-  position: fixed;
-  left: 0;
-  top: 0;
-  overflow: hidden;
-  width: 100%;
-  margin: 0;
-}
-
-.menu > li {
-  width: 20%; /*20*5=100%*/
-  /*float: right;*/
-  display: inline-grid;
-  float: right;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  /*margin-top: 15px;*/
-  line-height: 70px;
-  font-weight: bold;
-  background-color: #00000000;
+  color: #2c3e50;
+  /*margin-top: 70px;*/
 }
 
-.menu a {
-  color: black;
-  display: block;
-}
-
-.submenu > li {
-  line-height: 50px;
-  background-color: purple;
-}
-
-.submenu {
-  height: 0;
-  overflow: hidden;
-}
-
-.menu > li:hover {
-  background-color: purple;
-  transition-duration: 0.5s;
-}
-
-.menu > li:hover .submenu {
-  height: 250px; /*서브메뉴 li한개의 높이 50*5*/
-  transition-duration: 1s;
+div.body {
+  /*margin-top: 70px;*/
 }
 
 .submenu li {
@@ -156,7 +100,7 @@ body {
 }
 
 .submenu > li:hover {
-  background: blue;
+  background: #a200ff;
   color: white;
 }
 
